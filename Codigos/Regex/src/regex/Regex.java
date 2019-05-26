11945243ee446ec6cb08f684019f664203dbe87a
Catalina <cbustoszurita@gmail.com>
2019-05-26 13:05:30 -0400

    avance en diccionario regex

---
+++ Codigos/Regex/src/regex/Regex.java
@@ -0,0 +1,13 @@
+
+package regex;
+import java.util.regex.*;
+
+public class Regex {
+
+  private static final String PALABRA = "(a-z)";  //cualquier letra de la 'a' a la 'z'
+  private static final String  ESPACIO= "\\b"; 
+     
+  private static final String DIGITOS= "\\d";   //numeros del 0 al 9     
+  
+    
+}
